f3cde83ac3a781a6beeaa849b6a35936561fab3c
Abel Morris Ben <javaprojects756@gmail.com>
2021-11-27 10:02:47 +0200

    vpn detection

---
+++ src/main/java/io/github/kingpep/security/Response.java
@@ -0,0 +1,47 @@
+package io.github.kingpep.security;
+
+import com.google.gson.annotations.SerializedName;
+
+
+/*
+ *
+ * @author kingether
+ */
+
+public class Response {
+    public String status;
+    public String msg;
+
+    @SerializedName("package")
+    public String getPackage;
+
+    public String remaining_requests;
+    public String ipaddress;
+
+    @SerializedName("host-ip")
+    public boolean hostip;
+
+    public String hostname;
+    public String org;
+
+    public CS country;
+    public CS subdivision;
+
+    public String city;
+    public String postal;
+
+    public latlon location;
+
+    public class CS {
+        public String name;
+        public String code;
+    }
+
+    public class latlon {
+        public double lat;
+
+        @SerializedName("long")
+        public double lon;
+    }
+
+}
